a15a9dc23d01d56cedcb33296b80093a0dfc6928
Nathan McGuire <trusty007@Nathans-MBP.gateway>
2015-11-15 19:38:05 +1100

    scrollview coded, images added. refinement needed.

--- TheWalkingScrollView/ViewController.swift
+++ TheWalkingScrollView/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let Height: CGFloat = 380
+    let Width: CGFloat = 221
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for var x = 1; x <= 5; x++ {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake(-Width + (Width * CGFloat(x)), 250, Width, Height)
+        }
+        
+        scrollView.contentSize = CGSizeMake(Width * 5, scrollView.frame.size.height)
+        
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
